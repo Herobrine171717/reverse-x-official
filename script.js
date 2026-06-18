95f3deef75b457492d7840aa5ae859a54cd0f6e3
Herobrine171717 <148557911+Herobrine171717@users.noreply.github.com>
2026-06-19 00:26:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function draw(){
 ctx.fillStyle = "rgba(0,0,0,0.05)";
 ctx.fillRect(0,0,canvas.width,canvas.height);
 
-ctx.fillStyle = "#ff0033";
+ctx.fillStyle = "rgba(255,0,60,0.8)";
 ctx.font = fontSize + "px monospace";
 
 for(let i=0;i<drops.length;i++){
